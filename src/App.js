708c6ef5869ae826c096bbd88baa4d8d78ce6294
reddy1561 <myselfreddy31@gmail.com>
2022-12-09 14:59:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React ,{useState} from 'react';
+import './App.css';
+
+function App() {
+  const [search,setSearch]=useState("")
+  fetch()
+  return (
+         <div>
+          <center>
+            <h4>food receipe App</h4>
+            <form>
+              <input type="text" value={search} placeholder="search.." onChange={(e)=> setSearch(e.target.value)}/>
+              <input type="submit" value="search"/>
+              {search}
+            </form>
+          </center>
+         </div>
+  );
+}
+
+export default App;
